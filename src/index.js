438478b4a838f54f387712ad028aae4f0df9acf0
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 09:16:56 -0600

    Removed constructor and set state

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import ReactDOM from 'react-dom';
 
 // App class component is 'extended' using React.Component to access its built-in methods
 class App extends React.Component {
-    // First function run in any class-based component
-    constructor(props) {
-        // Allows use of React.Component methods after new constructor function overrides original React.Component
-        super(props);
-
-        // Set state for new object with empty, unknown value
-        this.state = {lat: null, errorMessage: ''};
-    }
+    state = {lat: null, errorMessage: ''};
 
     // === Lifecycle Methods ===
     componentDidMount() {

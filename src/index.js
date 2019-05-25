f6dd92c99f3a33c65be8178922f6e34ae84d9941
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 07:26:12 -0600

    Refactored App component to be a class-based component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    // Use Geolocation API to determine user's current location
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
 
-    return (
-        <div>Things rendered OK :)</div>
-    )
+// App class component is 'extended' using React.Component to access its built-in methods
+class App extends React.Component {
+    render(){
+        // Use Geolocation API to determine user's current location
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return (
+            <div>Latitude: </div>
+        )
+    };
 }
 
 ReactDOM.render(

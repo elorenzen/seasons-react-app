49a0e98b096a5b965e6b98f9cb259ba7541edcf1
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 07:39:38 -0600

    Added basic constructor function inside App class component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 
 // App class component is 'extended' using React.Component to access its built-in methods
 class App extends React.Component {
-    render(){
+    // First function run in any class-based component
+    constructor(props) {
+        // Allows use of React.Component methods after new constructor function overrides original React.Component
+        super(props);
+    }
+
+    // Render required for every React component
+    render() {
         // Use Geolocation API to determine user's current location
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
